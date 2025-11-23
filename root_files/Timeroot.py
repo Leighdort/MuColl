58baf6d1ba9875275dd9836239eb1bb4e01fe530
rldohert <regan_doherty@brown.edu>
2025-11-23 16:37:41 -0500

    Changing scripts

--- root_files/Timeroot.py
+++ root_files/Timeroot.py
@@ -20,7 +20,7 @@ system2name = {
 }
 
 for num in energies:
-    file = uproot.open(f"reco_outputp{num}.edm4hep.root")
+    file = uproot.open(f"reco_outpute{num}.edm4hep.root")
     events = file["events"]
 
     times_per_system = {name: [] for name in system2name.values()}
@@ -44,14 +44,58 @@ for num in energies:
 
     # ECAL histogram
 # Make log-spaced bins
-    bins = np.logspace(np.log10(ecal_times.min()), np.log10(ecal_times.max()), 50)
+    bins = np.logspace(np.log10(ecal_times.min()), np.log10(hcal_times.max()), 100)
+    plt.hist(ecal_times, bins=bins, alpha = 0.6, color = 'red', label = "ecal_times")
+    plt.hist(hcal_times, bins=bins, alpha = 0.6, color = 'blue', label= "hcal_times")
+    plt.xlabel("Times")
+    plt.ylabel("Counts")
+    plt.title(f"ECAL and HCAL, {num} GeV Electrons ")
+    plt.xscale('log')
+    plt.tight_layout()
+    plt.legend()
+    plt.savefig(f"time_hits{num}e.pdf")
+    plt.close()
+#Now we are going to have 2 different y-axis
+    fig, ax1 = plt.subplots()
+    h1 = ax1.hist(ecal_times, bins=bins, alpha=0.6, color='red', label = "ecal_times")
+    ax1.set_xlabel("Times")
+    ax1.set_ylabel("ECAL Counts", color='red')
+    ax1.tick_params(axis='y', labelcolor='red')
+    ax1.set_xscale('log')
+    ax2 = ax1.twinx()
+    h2 = ax2.hist(hcal_times, bins=bins, alpha = 0.6, color = 'blue', label= "hcal_times")
+    ax2.set_ylabel("HCAL Counts", color='blue')
+    ax2.tick_params(axis='y', labelcolor='blue')
+    plt.title(f"ECAL and HCAL, {num} GeV Electrons")
+    fig.tight_layout()
+    plt.savefig(f"time_hits_rescale{num}e.pdf")
+    plt.close()
+#We are going to have 2 different y-axis but have the focus region be 0 to 20
+    fig, ax1 = plt.subplots()
+    h1 = ax1.hist(ecal_times, bins=bins, alpha=0.6, color='red', label = "ecal_times")
+    ax1.set_xlabel("Times")
+    ax1.set_ylabel("ECAL Counts", color='red')
+    ax1.tick_params(axis='y', labelcolor='red')
+    ax1.set_xscale('log')
+    ax2 = ax1.twinx()
+    h2 = ax2.hist(hcal_times, bins=bins, alpha = 0.6, color = 'blue', label= "hcal_times")
+    ax2.set_ylabel("HCAL Counts", color='blue')
+    ax2.tick_params(axis='y', labelcolor='blue')
+    plt.title(f"ECAL and HCAL, {num} GeV Electrons")
+    fig.tight_layout()
+    ax1.set_xlim(0.9, 20)
+    plt.savefig(f"time_hits_rescale_lim{num}e.pdf")
+    plt.close() 
+
+    '''
     plt.hist(ecal_times, bins=bins, alpha=0.7, color='blue')
+    plt.hist
     plt.xlabel("Time")
     plt.ylabel("Counts")
-    plt.title(f"ECAL time distribution, {num} GeV")
+    plt.title(f"ECAL time distribution, {num} GeV Electrons")
     plt.xscale('log')  # keep axis in actual time units
     plt.tight_layout()
-    plt.savefig(f"time_hits_ecal{num}p.pdf")
+    plt.savefig(f"time_hits{num}3.pdf")
     plt.close()
 
     bins = np.logspace(np.log10(hcal_times.min()), np.log10(hcal_times.max()), 50)
@@ -63,3 +107,4 @@ for num in energies:
     plt.tight_layout()
     plt.savefig(f"time_hits_hcal{num}p.pdf")
     plt.close()
+'''
